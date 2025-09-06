261619e5bb27595453e2f940738f8d727e5f0321
Shivam Kumar <shivamkumar2687@gmail.com>
2025-09-06 23:44:41 +0530

    INtial Commit

---
+++ InternDisha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InternDisha
+//
+//  Created by Shivam Kumar on 06/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InternDisha/InternDishaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternDishaApp.swift
+//  InternDisha
+//
+//  Created by Shivam Kumar on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct InternDishaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
